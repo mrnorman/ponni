42ccdfb2783c58f6970a173e96738d922d5de972
Matt Norman <normanmr@ornl.gov>
2022-05-10 16:20:00 -0400

    new workflow is working with CUDA now. need a recent version, though. I'm using 11.6 for this.

--- src/ponni_load_keras.h
+++ src/ponni_load_keras.h
@@ -8,117 +8,20 @@
 
 namespace ponni {
 
-
-  template <int MAX_LAYERS>
-  inline void load_layer(Json::Value const &layer      ,
-                         int &num_prev_outputs         ,
-                         Sequential<MAX_LAYERS> &model ,
-                         hid_t &file                   ) {
-    auto class_name = layer["class_name"].asString();
-    if (class_name == "InputLayer") {
-
-      num_prev_outputs = layer["config"]["batch_input_shape"][1].asInt();
-
-    } else if (class_name == "Dense") {
-
-      auto name = layer["config"]["name"].asString();
-      int num_inputs  = num_prev_outputs;
-      int num_outputs = layer["config"]["units"].asInt();
-
-      // Open the HDF5 group
-      std::string group_name = std::string("/")+name+std::string("/")+name;
-      auto g_id = H5Gopen( file , group_name.c_str() , H5P_DEFAULT );
-
-      // Open and read the HDF5 kernel dataset
-      yakl::Array<float,2,yakl::memHost,yakl::styleC> kernel("kernel_file",num_inputs,num_outputs);
-      auto d_id = H5Dopen( g_id , "kernel:0" , H5P_DEFAULT );
-      H5Dread( d_id , H5T_NATIVE_FLOAT , H5S_ALL , H5S_ALL , H5P_DEFAULT , kernel.data() );
-      H5Dclose( d_id );
-
-      // Open and read the HDF5 bias dataset
-      yakl::Array<float,1,yakl::memHost,yakl::styleC> bias("bias",num_outputs);
-      d_id = H5Dopen( g_id , "bias:0" , H5P_DEFAULT );
-      H5Dread( d_id , H5T_NATIVE_FLOAT , H5S_ALL , H5S_ALL , H5P_DEFAULT , bias.data() );
-      H5Dclose( d_id );
-
-      // Create and add the matmul layer
-      {
-        Layer mylayer;
-        mylayer.init( TYPE_DENSE_MATMUL , num_inputs , num_outputs , kernel.collapse().createDeviceCopy() );
-        model.add_layer( mylayer );
-        num_prev_outputs = num_outputs;
-      }
-
-      // Create and add the bias layer
-      if (layer["config"]["use_bias"].asBool()) {
-        Layer mylayer;
-        mylayer.init( TYPE_DENSE_ADD_BIAS , num_outputs , num_outputs , bias.createDeviceCopy() );
-        model.add_layer( mylayer );
-      }
-
-      // Add an activation layer
-      if (layer["config"]["activation"] != "linear") {
-        load_layer( layer["config"]["activation"] , num_prev_outputs , model , file );
-      }
-
-    } else if (class_name == "LeakyReLU") {
-
-      auto name = layer["config"]["name"].asString();
-      int   num_inputs  = num_prev_outputs;
-      int   num_outputs = num_inputs;
-      float alpha       = layer["config"]["alpha"].asFloat();
-      yakl::Array<float,1,yakl::memHost,yakl::styleC> params("params",3);
-      params(0) = std::numeric_limits<real>::max();
-      params(1) = alpha;
-      params(2) = 0;
-      Layer mylayer;
-      mylayer.init( TYPE_ACT_RELU , num_inputs , num_outputs , params.createDeviceCopy() );
-      model.add_layer( mylayer );
-
-    } else if (class_name == "ReLU") {
-
-      auto name = layer["config"]["name"].asString();
-      int   num_inputs  = num_prev_outputs;
-      int   num_outputs = num_inputs;
-      yakl::Array<float,1,yakl::memHost,yakl::styleC> params("params",3);
-      real max_value      = layer["config"]["max_value"     ].asFloat();
-      real negative_slope = layer["config"]["negative_slope"].asFloat();
-      real threshold      = layer["config"]["threshold"     ].asFloat();
-      if (max_value == 0) max_value = std::numeric_limits<real>::max();
-      params(0) = max_value;
-      params(1) = negative_slope;
-      params(2) = threshold;
-      Layer mylayer;
-      mylayer.init( TYPE_ACT_RELU , num_inputs , num_outputs , params.createDeviceCopy() );
-      model.add_layer( mylayer );
-
-    } else {
-
-      std::cout << "Error: Don't recognize the layer class_name\n";
-
-    }
-  }
-
-
-
-  template <int MAX_LAYERS>
-  inline Sequential<MAX_LAYERS> load_keras_model( std::string model_json , std::string weights_h5 ) {
-    Json::Value root;
-    std::ifstream file_in( model_json );
-    file_in >> root;
-
-    auto file = H5Fopen( weights_h5.c_str() , H5F_ACC_RDONLY , H5P_DEFAULT );
-
-    Sequential<MAX_LAYERS> model;
-
-    auto layers = root["config"]["layers"];
-    int num_prev_outputs;
-
-    for (auto &layer : layers) { load_layer( layer , num_prev_outputs , model , file ); }
-
-    H5Fclose( file );
-
-    return model;
+  template <int N>
+  inline void load_weights(yakl::Array<float,N,yakl::memDevice,yakl::styleC> const &weights ,
+                           std::string file_name   ,
+                           std::string group_name  ,
+                           std::string dataset_name ) {
+    auto f_id = H5Fopen( file_name.c_str() , H5F_ACC_RDONLY , H5P_DEFAULT );
+    auto g_id = H5Gopen( f_id , group_name  .c_str() , H5P_DEFAULT );
+    auto d_id = H5Dopen( g_id , dataset_name.c_str() , H5P_DEFAULT );
+    yakl::Array<float,N,yakl::memHost,yakl::styleC> weights_host = weights.createHostObject();
+    H5Dread( d_id , H5T_NATIVE_FLOAT , H5S_ALL , H5S_ALL , H5P_DEFAULT , weights_host.data() );
+    weights_host.deep_copy_to(weights);
+    H5Dclose( d_id );
+    H5Gclose( g_id );
+    H5Fclose( f_id );
   }
 
 }
--- src/test.cpp
+++ src/test.cpp
@@ -1,14 +1,31 @@
 
 #include "ponni.h"
+#include "ponni_load_keras.h"
 
 int main( int argc , char **argv ) {
   yakl::init();
   {
-    ponni::Matvec matvec( ponni::real2d("weights",12,10) );
-    ponni::Bias   bias  ( ponni::real1d("weights",10) );
-    ponni::Relu   relu  ( 10, 0.1 );
+    std::string fname = "supercell_micro_Keras_modelwt_NORMip_NORMop1000000_Nneu10.h5";
 
-    auto model = ponni::create_model( matvec , bias , relu );
+    ponni::real2d matvec_1_weights("matvec_1_weights",12,10);
+    ponni::load_weights( matvec_1_weights , fname , "/dense/dense" , "kernel:0" );
+    ponni::Matvec matvec_1( matvec_1_weights );
+
+    ponni::real1d bias_1_weights("bias_1_weights",10);
+    ponni::load_weights( bias_1_weights , fname , "/dense/dense" , "bias:0" );
+    ponni::Bias bias_1( bias_1_weights );
+
+    ponni::Relu relu_1( 10, 0.1 );
+
+    ponni::real2d matvec_2_weights("matvec_2_weights",10,4);
+    ponni::load_weights( matvec_2_weights , fname , "/dense_1/dense_1" , "kernel:0" );
+    ponni::Matvec matvec_2( matvec_2_weights );
+
+    ponni::real1d bias_2_weights("bias_2_weights",4);
+    ponni::load_weights( bias_2_weights , fname , "/dense_1/dense_1" , "bias:0" );
+    ponni::Bias bias_2( bias_2_weights );
+
+    auto model = ponni::create_model( matvec_1 , bias_1 , relu_1 , matvec_2 , bias_2 );
     model.print_verbose();
 
     yakl::Array<float,2,yakl::memHost,yakl::styleC> inputs("inputs",12,1);
@@ -30,6 +47,7 @@ int main( int argc , char **argv ) {
     std::cout << outputs;
 
     // 4.7658795e-01 4.8446856e-02 1.2472458e-03 4.0419400e-05
+
   }
   yakl::finalize();
 }
